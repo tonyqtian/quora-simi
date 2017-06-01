afd634a6a06d24c04fe021c4a10d4fe44fcd664b
tonyqtian <tonyqtian@gmail.com>
2017-06-01 16:35:21 +0800

    a new corrector

---
+++ test/csvCorrector.py
@@ -0,0 +1,23 @@
+'''
+Created on Jun 1, 2017
+
+@author: tonyq
+'''
+import pandas as pd
+from tqdm._tqdm import tqdm
+import csv
+
+
+train = pd.read_csv("../output/0601/20170601-144724-XGB_leaky.csv")
+totalen = len(train.is_duplicate)
+print('Total size: ', totalen)
+
+fulllist = zip(train.test_id, train.is_duplicate)
+length = len(train.question1)
+del train
+
+with open("../output/0601/20170601-144724-XGB_leaky.clean.csv", "w", encoding='utf8') as fwrt:
+	writer_sub = csv.writer(fwrt)
+	writer_sub.writerow(['test_id','is_duplicate'])
+	for (theid, dup) in tqdm(fulllist, total=length):
+		writer_sub.writerow([theid, dup])
