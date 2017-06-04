c73e5483d9783a06616101c69c6a1b796e970aea
tonyqtian <tonyqtian@gmail.com>
2017-06-04 18:27:25 +0800

    updated cleaner filename

--- test/csvCorrector.py
+++ test/csvCorrector.py
@@ -8,7 +8,7 @@ from tqdm._tqdm import tqdm
 import csv
 
 
-train = pd.read_csv("../output/0601/20170601-144724-XGB_leaky.csv")
+train = pd.read_csv("../output/0604/20170604-165432-XGB_leaky.csv")
 totalen = len(train.is_duplicate)
 print('Total size: ', totalen)
 
@@ -16,7 +16,7 @@ fulllist = zip(train.test_id, train.is_duplicate)
 length = len(train.is_duplicate)
 del train
 
-with open("../output/0601/20170601-144724-XGB_leaky.clean.csv", "w", encoding='utf8') as fwrt:
+with open("../output/0604/20170604-165432-XGB_leaky.clean.csv", "w", encoding='utf8') as fwrt:
 	writer_sub = csv.writer(fwrt)
 	writer_sub.writerow(['test_id','is_duplicate'])
 	for (theid, dup) in tqdm(fulllist, total=length):
