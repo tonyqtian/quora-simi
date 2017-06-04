95f66e4bfb3a38d26f40b6b180cadf3c3e81cc83
tonyqtian <tonyqtian@gmail.com>
2017-06-04 23:56:45 +0800

    new stucture: added embd mean over time

--- src/rnn_model.py
+++ src/rnn_model.py
@@ -4,8 +4,7 @@ Created on Mar 18, 2017
 @author: tonyq
 '''
 import logging
-from keras.models import Sequential
-# from keras.layers import Dense
+from random import random
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 from keras.layers.core import Dropout
@@ -14,15 +13,16 @@ from keras.layers.merge import Concatenate
 from keras.engine.topology import Input
 from keras.models import Model
 
-from util.my_layers import DenseWithMasking
 from keras.layers.normalization import BatchNormalization
-from random import random
+# from keras.layers import Dense
+from util.my_layers import DenseWithMasking
+from util.my_layers import MeanOverTime
 
 logger = logging.getLogger(__name__)
 
 mem_opt_dict = {'cpu':0, 'mem':1, 'gpu': 2}
 
-def getModel(args, input_length, vocab_size, embd, embd_trainable=True, feature_length=0):
+def getModel(args, input_length, vocab_size, embd, feature_length=0):
 	rnn_opt = mem_opt_dict[args.rnn_opt]
 	rnn_dim = args.rnn_dim
 	dropout_prob = args.dropout_prob + random() * 0.15
@@ -30,47 +30,58 @@ def getModel(args, input_length, vocab_size, embd, embd_trainable=True, feature_
 # 		final_init = 'he_normal'
 # 	else:
 # 		final_init = 'he_uniform'
-	
-	rnn_model = Sequential()
+		
 	if type(embd) is type(None):
-		rnn_model.add(Embedding(vocab_size, args.embd_dim, mask_zero=True, trainable=embd_trainable))
+		embd_layer = Embedding(vocab_size, args.embd_dim, mask_zero=True, trainable=True)
 	else:
-		rnn_model.add(Embedding(vocab_size, args.embd_dim, mask_zero=True, weights=[embd], 
-							trainable=embd_trainable))
-	# hidden rnn layer
-	for _ in range(args.rnn_layer-1):
-		if args.bidirectional:
-			rnn_model.add(Bidirectional(LSTM(rnn_dim, return_sequences=True, implementation=rnn_opt, 
-											dropout=dropout_prob, recurrent_dropout=dropout_prob)))
-		else:
-			rnn_model.add(LSTM(rnn_dim, return_sequences=True, implementation=rnn_opt, 
-							dropout=dropout_prob, recurrent_dropout=dropout_prob))
-
-	# output rnn layer	
-	if args.attention:
-		from util.attention_wrapper import Attention		
-		rnn_model.add(Attention(LSTM(rnn_dim, return_sequences=False, implementation=rnn_opt, 
-									dropout=dropout_prob, recurrent_dropout=dropout_prob)))
+		embd_layer = Embedding(vocab_size, args.embd_dim, mask_zero=True, weights=[embd], trainable=False)
+		
+	if args.bidirectional:
+		rnn_layer = Bidirectional(LSTM(rnn_dim, return_sequences=False, implementation=rnn_opt, 
+										dropout=dropout_prob, recurrent_dropout=dropout_prob))
 	else:
-		if args.bidirectional:
-			rnn_model.add(Bidirectional(LSTM(rnn_dim, return_sequences=False, implementation=rnn_opt,
-											 dropout=dropout_prob, recurrent_dropout=dropout_prob)))
-		else:
-			rnn_model.add(LSTM(rnn_dim, return_sequences=False, implementation=rnn_opt, 
-							dropout=dropout_prob, recurrent_dropout=dropout_prob))
+		rnn_layer = LSTM(rnn_dim, return_sequences=False, implementation=rnn_opt, 
+						dropout=dropout_prob, recurrent_dropout=dropout_prob)
+# 	# hidden rnn layer
+# 	from keras.models import Sequential
+# 	rnn_model = Sequential()
+# 	for _ in range(args.rnn_layer-1):
+# 		if args.bidirectional:
+# 			rnn_model.add(Bidirectional(LSTM(rnn_dim, return_sequences=True, implementation=rnn_opt, 
+# 											dropout=dropout_prob, recurrent_dropout=dropout_prob)))
+# 		else:
+# 			rnn_model.add(LSTM(rnn_dim, return_sequences=True, implementation=rnn_opt, 
+# 							dropout=dropout_prob, recurrent_dropout=dropout_prob))
+# 
+# 	# output rnn layer	
+# 	if args.attention:
+# 		from util.attention_wrapper import Attention		
+# 		rnn_model.add(Attention(LSTM(rnn_dim, return_sequences=False, implementation=rnn_opt, 
+# 									dropout=dropout_prob, recurrent_dropout=dropout_prob)))
+# 	else:
+# 		if args.bidirectional:
+# 			rnn_model.add(Bidirectional(LSTM(rnn_dim, return_sequences=False, implementation=rnn_opt,
+# 											 dropout=dropout_prob, recurrent_dropout=dropout_prob)))
+# 		else:
+# 			rnn_model.add(LSTM(rnn_dim, return_sequences=False, implementation=rnn_opt, 
+# 							dropout=dropout_prob, recurrent_dropout=dropout_prob))
 	
 	sequence_1_input = Input(shape=(input_length,), dtype='int32')
 	sequence_2_input = Input(shape=(input_length,), dtype='int32')
-	vec1 = rnn_model(sequence_1_input)
-	vec2 = rnn_model(sequence_2_input)
+	vec1 = embd_layer(sequence_1_input)
+	vec2 = embd_layer(sequence_2_input)
+	vec1_w2v = MeanOverTime()(vec1)
+	vec2_w2v = MeanOverTime()(vec2)
+	vec1_rnn = rnn_layer(vec1)
+	vec2_rnn = rnn_layer(vec2)
 	
 	if feature_length:
 		feature_input = Input(shape=(feature_length,), dtype='float32')
-		featured = BatchNormalization()(feature_input)
-		featured = DenseWithMasking(feature_length*3//5, kernel_initializer='he_uniform', activation='relu')(featured)
-		merged = Concatenate()([vec1, vec2, featured])
+		featured = DenseWithMasking(feature_length*3//5, kernel_initializer='he_uniform', 
+								activation='relu')(feature_input)
+		merged = Concatenate()([vec1_rnn, vec1_w2v, vec2_rnn, vec2_w2v, featured])
 	else:	
-		merged = Concatenate()([vec1, vec2])
+		merged = Concatenate()([vec1_rnn, vec1_w2v, vec2_rnn, vec2_w2v])
 	merged = BatchNormalization()(merged)
 	merged = Dropout(dropout_prob)(merged)
 
--- util/my_layers.py
+++ util/my_layers.py
@@ -4,6 +4,10 @@ Created on Mar 24, 2017
 @author: tonyq
 '''
 from keras.layers.core import Dense
+from keras.layers.pooling import AveragePooling1D
+from keras.layers.convolutional import Convolution1D
+from keras.engine.topology import Layer
+from keras.backend import sum, cast, floatx, mean
 
 class DenseWithMasking(Dense):
 	def __init__(self, output_dim, **kwargs):
@@ -12,3 +16,46 @@ class DenseWithMasking(Dense):
 	
 	def compute_mask(self, x, mask=None):
 		return None
+
+class AveragePooling1DWithMasking(AveragePooling1D):
+	def __init__(self, pool_size, **kwargs):
+		self.supports_masking = True
+		super(AveragePooling1DWithMasking, self).__init__(pool_size, **kwargs)
+	
+	def compute_mask(self, x, mask=None):
+		return None
+			
+class Conv1DWithMasking(Convolution1D):
+	def __init__(self, **kwargs):
+		self.supports_masking = True
+		super(Conv1DWithMasking, self).__init__(**kwargs)
+	
+	def compute_mask(self, x, mask):
+		return mask
+	
+class MeanOverTime(Layer):
+	def __init__(self, mask_zero=True, **kwargs):
+		self.mask_zero = mask_zero
+		self.supports_masking = True
+		super(MeanOverTime, self).__init__(**kwargs)
+
+	def call(self, x, mask=None):
+		if self.mask_zero:
+			return sum(x, axis=1) / sum(cast(mask, floatx()), axis=1, keepdims=True)
+		else:
+			return mean(x, axis=1)
+
+	def get_output_shape_for(self, input_shape):
+		return (input_shape[0], input_shape[2])
+	
+	def compute_output_shape(self, input_shape):
+		return (input_shape[0], input_shape[2])
+	
+	def compute_mask(self, x, mask):
+		return None
+	
+	def get_config(self):
+		config = {'mask_zero': self.mask_zero}
+		base_config = super(MeanOverTime, self).get_config()
+		return dict(list(base_config.items()) + list(config.items()))
+	
