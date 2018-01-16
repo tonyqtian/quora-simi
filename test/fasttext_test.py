0202c801f37b0f5e4f3bf02d3d1c7402bc055adc
Tian Qiu <tony.qiu@liulishuo.com>
2018-01-16 10:27:28 +0800

    added fasttext test

--- test/fasttext_test.py
+++ test/fasttext_test.py
@@ -47,7 +47,7 @@ print(model_gensim)
 with open(data_dir + 'questions_file.txt', 'w') as fw:
     for line in train_data:
         fw.write(line + '\n')
-print('text saved to %s' % (data_dir + 'questions_file.txt'))
+print('Text saved to %s' % (data_dir + 'questions_file.txt'))
 
 # train the model
 print('Training wrapper fasttext model...')
@@ -55,27 +55,34 @@ model_wrapper = FT_wrapper.train(ft_home, data_dir + 'questions_file.txt')
 print(model_wrapper)
 
 # saving a model trained via Gensim's fastText implementation
+print('Loading fasttext gensim model...')
 model_gensim.save(output_dir + 'saved_model_gensim')
 loaded_model = FT_gensim.load(output_dir + 'saved_model_gensim')
 print(loaded_model)
 
 # saving a model trained via fastText wrapper
+print('Loading fasttext wrapper model...')
 model_wrapper.save(output_dir + 'saved_model_wrapper')
 loaded_model = FT_wrapper.load(output_dir + 'saved_model_wrapper')
 print(loaded_model)
 
+print('night and nights')
 print('night' in model_wrapper.wv.vocab)
 print('nights' in model_wrapper.wv.vocab)
+print('night vec:')
 print(model_wrapper['night'])
+print('nights vec:')
 print(model_wrapper['nights'])
-model_wrapper.similarity("night", "nights")
-model_wrapper.most_similar("nights")
 
-model_wrapper.n_similarity(['sushi', 'shop'], ['japanese', 'restaurant'])
+print('night and nights similarity: %r' % model_wrapper.similarity("night", "nights"))
+print('most similar to nights: ')
+print(model_wrapper.most_similar("nights"))
 
-model_wrapper.doesnt_match("breakfast cereal dinner lunch".split())
+print('n similarity %r' % model_wrapper.n_similarity(['sushi', 'shop'], ['japanese', 'restaurant']) )
 
-model_wrapper.most_similar(positive=['baghdad', 'england'], negative=['london'])
+print('doesnt match %r' % model_wrapper.doesnt_match("breakfast cereal dinner lunch".split()) )
+
+print('most similar %r' % model_wrapper.most_similar(positive=['baghdad', 'england'], negative=['london']) )
 
 # Word Movers distance
 sentence_obama = 'Obama speaks to the media in Illinois'.lower().split()
@@ -89,4 +96,4 @@ sentence_president = [w for w in sentence_president if w not in stopwords]
 
 # Compute WMD.
 distance = model_wrapper.wmdistance(sentence_obama, sentence_president)
-print(distance)
+print('distance %.4f' % distance)
